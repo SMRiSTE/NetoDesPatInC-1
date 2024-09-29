032f0fb2a148ac62a5459d1dcfb38a5d4dc4a145
SMRist <s.m.r.00800ist@gmail.com>
2024-09-29 23:13:20 +0300

    Initial commit

---
+++ 1/1/1.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+class SqlSelectQueryBuilder {
+public:
+    SqlSelectQueryBuilder() : table_(""), has_columns_(false) {}
+
+    SqlSelectQueryBuilder& AddColumn(const std::string& column) {
+        has_columns_ = true;
+        columns_.push_back(column);
+        return *this;
+    }
+
+    SqlSelectQueryBuilder& AddFrom(const std::string& table) {
+        table_ = table;
+        return *this;
+    }
+
+    SqlSelectQueryBuilder& AddWhere(const std::string& key, const std::string& value) {
+        where_conditions_.push_back(key + "=" + value);
+        return *this;
+    }
+
+    std::string BuildQuery() const {
+        std::ostringstream query;
+
+        query << "SELECT ";
+
+        if (columns_.empty()) {
+            query << "*";
+        }
+        else {
+            query << Join(columns_, ", ");
+        }
+
+        if (!table_.empty()) {
+            query << " FROM " << table_;
+        }
+
+        if (!where_conditions_.empty()) {
+            query << " WHERE " << Join(where_conditions_, " AND ");
+        }
+
+        query << ";";  // Завершаем запрос
+
+        return query.str();
+    }
+
+private:
+    std::string table_;
+    std::vector<std::string> columns_;
+    std::vector<std::string> where_conditions_;
+    bool has_columns_;
+
+    std::string Join(const std::vector<std::string>& elements, const std::string& separator) const {
+        std::ostringstream joined;
+        for (size_t i = 0; i < elements.size(); ++i) {
+            joined << elements[i];
+            if (i < elements.size() - 1) {
+                joined << separator;
+            }
+        }
+        return joined.str();
+    }
+};
+
+int main() {
+    SqlSelectQueryBuilder query_builder;
+    query_builder.AddColumn("name").AddColumn("phone");
+    query_builder.AddFrom("students");
+    query_builder.AddWhere("id", "42").AddWhere("name", "John");
+
+    std::string query = query_builder.BuildQuery();
+    std::cout << query << std::endl;  // Вывод: SELECT name, phone FROM students WHERE id=42 AND name=John;
+
+    return 0;
+}
---
+++ 2/2/2.cpp
@@ -0,0 +1,87 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include<map>
+#include <sstream>
+
+class SqlSelectQueryBuilder {
+public:
+    SqlSelectQueryBuilder() : table_(""), has_columns_(false) {}
+
+    SqlSelectQueryBuilder& AddColumn(const std::vector<std::string>& colums) {
+        has_columns_ = true;
+        for (int i = 0; i < colums.size(); i++) {
+            columns_.push_back(colums[i]);
+        }
+        return *this;
+    }
+
+    SqlSelectQueryBuilder& AddFrom(const std::string& table) {
+        table_ = table;
+        return *this;
+    }
+
+    SqlSelectQueryBuilder& AddWhere(const std::map<std::string, std::string>& kv) {
+        for (std::map<std::string, std::string>::const_iterator it = kv.begin(); it != kv.end(); ++it) {
+            where_conditions_.push_back(it->first + "=" + it->second);
+        }
+        return *this;
+    }
+
+    std::string BuildQuery() const {
+        std::ostringstream query;
+
+        query << "SELECT ";
+
+        if (columns_.empty()) {
+            query << "*";
+        }
+        else {
+            query << Join(columns_, ", ");
+        }
+
+        if (!table_.empty()) {
+            query << " FROM " << table_;
+        }
+
+        if (!where_conditions_.empty()) {
+            query << " WHERE " << Join(where_conditions_, " AND ");
+        }
+
+        query << ";";
+
+        return query.str();
+    }
+
+private:
+    std::string table_;
+    std::vector<std::string> columns_;
+    std::vector<std::string> where_conditions_;
+    bool has_columns_;
+
+    std::string Join(const std::vector<std::string>& elements, const std::string& separator) const {
+        std::ostringstream joined;
+        for (size_t i = 0; i < elements.size(); ++i) {
+            joined << elements[i];
+            if (i < elements.size() - 1) {
+                joined << separator;
+            }
+        }
+        return joined.str();
+    }
+};
+
+int main() {
+    std::vector<std::string> vec = { "name","phone","email", "city"};
+    std::map<std::string, std::string> map = { {"id","42"}, {"name", "John"} };
+
+    SqlSelectQueryBuilder query_builder;
+    query_builder.AddColumn(vec);
+    query_builder.AddFrom("students");
+    query_builder.AddWhere(map);
+
+    std::string query = query_builder.BuildQuery();
+    std::cout << query << std::endl;
+
+    return 0;
+}
